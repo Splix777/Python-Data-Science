76e55972ffa110fd51a3cc418538b79bffb2b2c2
splix <federicosalalois@gmail.com>
2024-02-20 12:39:21 +0100

    ex06

--- Python-0-Starting/ex06/filterstring.py
+++ Python-0-Starting/ex06/filterstring.py
@@ -1,3 +1,26 @@
 # filterstring.py
 
 import sys
+
+
+def main():
+    '''
+    - Words are separated from each other by space characters.
+    - Strings do not contain any special characters.
+      (Punctuation or invisible)
+    - The program must contain at least one list comprehension
+      expression and one lambda.
+    - If the number of argument is different from 2, or if the type
+      of any argument is wrong, the program prints an AssertionError.
+    '''
+    if len(sys.argv) != 3:
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
+    s = sys.argv[1]
+    n = int(sys.argv[2])
+    # print(list(filter(lambda x: len(x) >= n, s.split())))
+    print([word for word in s.split() if (lambda x: len(x) >= n)(word)])
+
+
+if __name__ == "__main__":
+    main()
--- Python-0-Starting/ex06/ft_filter.py
+++ Python-0-Starting/ex06/ft_filter.py
@@ -1,21 +1,45 @@
 # ft_filter.py
 
-import sys
-
-
 def ft_filter(function, iterable):
     """
-    This function is a copy of the built-in filter function.
-    It returns an iterator yielding those items of iterable for which function(item)
-    is true. If function is None, return the items that are true.
+    ft_filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable
+    for which function(item) is true. If function is None,
+    return the items that are true.
     """
     if function is None:
-        return (item for item in iterable if item)
-    return (item for item in iterable if function(item))
+        function = bool
+    for item in iterable:
+        if function(item):
+            yield item
+
 
 def main():
+    print('ft_filter.py')
     print(ft_filter.__doc__)
+    print('filter.py')
     print(filter.__doc__)
+
+    example_string = "Hello, World! 123"
+
+    print("\nTesting ft_filter function:")
+    print(f"Original string: {example_string}")
+    print(
+        f"Filtered string using ft_filter: "
+        f"{''.join(ft_filter(str.isalpha, example_string))}")
+    print(
+        f"Filtered string using ft_filter:"
+        f"{''.join(ft_filter(None, example_string))}")
+
+    print("\nTesting filter function:")
+    print(f"Original string: {example_string}")
+    print(
+        f"Filtered string using filter:"
+        f"{''.join(filter(str.isalpha, example_string))}")
+    print(
+        f"Filtered string using filter:"
+        f"{''.join(filter(None, example_string))}")
 
 
 if __name__ == '__main__':
